6901cb2e77a5297c0e3a002ada1fec4bc6a1bb18
m-yordanov <114444124+m-yordanov@users.noreply.github.com>
2024-03-21 03:18:25 +0200

    Updated the AttendanceController

--- Controllers/AttendanceController.cs
+++ Controllers/AttendanceController.cs
@@ -1,33 +1,34 @@
-﻿//using LearnLink.Data;
-//using LearnLink.Models; 
-//using Microsoft.AspNetCore.Mvc;
+﻿using LearnLink.Data;
+using LearnLink.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-//namespace LearnLink.Controllers
-//{
-//    public class AttendanceController : Controller
-//    {
-//        private readonly LearnLinkDbContext data;
+namespace LearnLink.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        private readonly LearnLinkDbContext data;
 
-//        public AttendanceController(LearnLinkDbContext context)
-//        {
-//            data = context;
-//        }
+        public AttendanceController(LearnLinkDbContext context)
+        {
+            data = context;
+        }
 
-//        public IActionResult Index()
-//        {
-//            var attendances = data.Attendances.ToList();
+        public async Task<IActionResult> All()
+        {
+            var attendances = await data.Attendances.ToListAsync();
 
-//            var attendanceViewModels = attendances.Select(a => new AttendanceViewModel
-//            {
-//                Id = a.Id,
-//                DateAndTime = a.DateAndTime,
-//                Status = a.Status,
-//                Subject = a.Subject.Name, 
-//                TeacherFirstName = a.Teacher.FirstName, 
-//                TeacherLastName = a.Teacher.LastName 
-//            });
+            var attendanceViewModels = attendances.Select(a => new AttendanceViewModel
+            {
+                Id = a.Id,
+                DateAndTime = a.DateAndTime,
+                Status = a.Status,
+                Subject = a.Subject.Name,
+                TeacherFirstName = a.Teacher.FirstName,
+                TeacherLastName = a.Teacher.LastName
+            });
 
-//            return View(attendanceViewModels);
-//        }
-//    }
-//}
+            return View(attendanceViewModels);
+        }
+    }
+}
