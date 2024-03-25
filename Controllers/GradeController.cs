7c835bf45be1f04910c210f14e884bdec1d742c4
m-yordanov <114444124+m-yordanov@users.noreply.github.com>
2024-03-25 03:24:20 +0200

    Added grade view functionality for students

--- Controllers/GradeController.cs
+++ Controllers/GradeController.cs
@@ -1,25 +1,37 @@
 ﻿using LearnLink.Data;
+using LearnLink.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 public class GradeController : Controller
 {
-    private readonly LearnLinkDbContext _context;
+    private readonly LearnLinkDbContext data;
 
     public GradeController(LearnLinkDbContext context)
     {
-        _context = context;
+        data = context;
     }
 
-    public IActionResult All()
+    public async Task<IActionResult> AllGrades()
     {
-        var studentName = User.Identity.Name;
-        var studentGrades = _context.Grades
-            .Include(g => g.Subject)
-            .Where(g => g.Student.IdentityUser.UserName == studentName)
-            .OrderBy(g => g.Subject.Name)
-            .ToList();
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        return View(studentGrades);
+        var studentGrades = await data.Grades
+            .Include(g => g.Subject)
+            .Include(g => g.Teacher)
+            .Where(g => g.Student.UserId == userId)
+            .Select(g => new GradeViewModel
+            {
+                Subject = g.Subject.Name,
+                Value = g.Value,
+                DateAndTime = g.DateAndTime,
+                TeacherFirstName = g.Teacher.FirstName,
+                TeacherLastName = g.Teacher.LastName
+            })
+            .ToListAsync();
+
+        return View(nameof(AllGrades), studentGrades);
     }
 }
+
