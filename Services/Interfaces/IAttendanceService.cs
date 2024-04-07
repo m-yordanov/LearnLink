9f52de1843412eeae9bc332632a63a20194b0159
m-yordanov <114444124+m-yordanov@users.noreply.github.com>
2024-04-08 02:45:24 +0300

    Reapply "Added filtering for all attendances view"
    This reverts commit 64cd99a82e04b51ae062f3a24ec45ddd85182189.

--- Services/Interfaces/IAttendanceService.cs
+++ Services/Interfaces/IAttendanceService.cs
@@ -5,5 +5,9 @@ namespace LearnLink.Services.Interfaces
     public interface IAttendanceService
     {
         Task<IEnumerable<AttendanceViewModel>> GetStudentAttendancesAsync(string studentId);
+
+        Task<IEnumerable<AttendanceViewModel>> GetFilteredAttendancesAsync(string selectedStudent, string selectedTeacher, string selectedSubject, string selectedStatus, DateTime? dateBefore, DateTime? dateAfter, int pageNumber, int pageSize);
+
+        Task<int> GetTotalFilteredAttendancesAsync(string selectedStudent, string selectedTeacher, string selectedSubject, DateTime? dateBefore, DateTime? dateAfter);
     }
 }
