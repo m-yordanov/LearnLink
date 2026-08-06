aae175e2fe2134db35f54b65a644047b5ec4c548
m-yordanov <114444124+m-yordanov@users.noreply.github.com>
2026-08-06 19:54:05 +0300

    New migrations

---
+++ LearnLink.Infrastructure/Data/Migrations/20260711120730_RestorationMigration.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnLink.Migrations
+{
+    public partial class RestorationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Teachers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Teachers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Students",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6d5800ce-d726-4fc8-83d9-d6b3ac1f592d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "e1f83c46-c1a5-4870-8447-b0b399035ac4", "AQAAAAEAACcQAAAAEHND0K+Y+rnCaFUzR+ussBps/28F7VBGNRvCXbOzv7mfCvU6622kNiFEdGGe1QPbTg==", "0db2227e-9f41-46a1-9df9-34e4290b622a" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "c2b15954-6a87-4207-8f3d-fb93ef5481f4",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "007e0918-b993-45de-a254-8053bc10a141", "AQAAAAEAACcQAAAAEPlrrhtGUUqffS0i23TiGtrM75PsCR59OF+/L/DrKCk4ari7AwheSuXtHYXAtyc14w==", "590871c2-bbf8-4e02-9f4e-7b81b5b0e139" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "dea12856-c098-4129-b3f3-b893d8395082",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "f2b24258-c192-4ef8-bfc1-b6c15d2ccf32", "AQAAAAEAACcQAAAAEPuDCnn2OViyOCZCJDPhBC7UM/7unLPkmHsM3stDLuG8Z+O47DRS/tp7YAYBP76D/w==", "25e4616e-4818-4d50-b4d4-000bbf56b53e" });
+
+            migrationBuilder.UpdateData(
+                table: "Attendances",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateAndTime",
+                value: new DateTime(2026, 7, 11, 15, 7, 30, 375, DateTimeKind.Local).AddTicks(2289));
+
+            migrationBuilder.UpdateData(
+                table: "Attendances",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateAndTime",
+                value: new DateTime(2026, 7, 11, 15, 7, 30, 375, DateTimeKind.Local).AddTicks(2313));
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateAndTime",
+                value: new DateTime(2026, 7, 11, 15, 7, 30, 371, DateTimeKind.Local).AddTicks(8536));
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateAndTime",
+                value: new DateTime(2026, 7, 11, 15, 7, 30, 371, DateTimeKind.Local).AddTicks(8539));
+
+            migrationBuilder.InsertData(
+                table: "Subjects",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Mathematics" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Teachers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Teachers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Students",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6d5800ce-d726-4fc8-83d9-d6b3ac1f592d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "620b6167-2d93-45b4-aaa2-4796949e7580", "AQAAAAEAACcQAAAAEBGhPe5Fn0ueJsHsgazuIOrFsCSQSMDMmqO0ff/q7Uud3KwHCxWb00k+QiTZ3mk38A==", "bfb0e7c5-7d99-45bb-b9f6-b500a9092ee1" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "c2b15954-6a87-4207-8f3d-fb93ef5481f4",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "b0bc2d19-5c4e-483a-b23e-40a7f140a160", "AQAAAAEAACcQAAAAEON02ygRLsniRhaSCECaVClpFE+RoN6xcbu487nXU9tfUon+AR+ttW2v25MW6mduaw==", "6432b5bb-a294-4161-ba4d-c55c22657bcc" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "dea12856-c098-4129-b3f3-b893d8395082",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2dda76f9-2bd8-47fd-99cb-09a1b1f95759", "AQAAAAEAACcQAAAAEBil0GkfAyRlH9RhmC4kG/plJtGX3IK9N77Q+bUPzI/uYoeztq21aXWyUjE35j2Isw==", "97f18cb3-219f-4095-8610-f730ad78daec" });
+
+            migrationBuilder.UpdateData(
+                table: "Attendances",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateAndTime",
+                value: new DateTime(2024, 4, 4, 23, 11, 27, 668, DateTimeKind.Local).AddTicks(5219));
+
+            migrationBuilder.UpdateData(
+                table: "Attendances",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateAndTime",
+                value: new DateTime(2024, 4, 4, 23, 11, 27, 668, DateTimeKind.Local).AddTicks(5241));
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateAndTime",
+                value: new DateTime(2024, 4, 4, 23, 11, 27, 665, DateTimeKind.Local).AddTicks(5083));
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateAndTime",
+                value: new DateTime(2024, 4, 4, 23, 11, 27, 665, DateTimeKind.Local).AddTicks(5084));
+        }
+    }
+}
