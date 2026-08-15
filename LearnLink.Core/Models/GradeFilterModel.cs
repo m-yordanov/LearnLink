5eba85b65379891a7001cda02b50d78024be9b47
m-yordanov <114444124+m-yordanov@users.noreply.github.com>
2026-08-15 11:08:03 +0300

    Filter code cleanup

--- LearnLink.Core/Models/GradeFilterModel.cs
+++ LearnLink.Core/Models/GradeFilterModel.cs
@@ -1,39 +1,6 @@
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using static LearnLink.Core.Constants.PaginationConstants;
-
 namespace LearnLink.Core.Models
 {
-    [ValidateNever]
-    public class GradeFilterModel
+    public class GradeFilterModel : ListFilterModel
     {
-        public string SelectedStudent { get; set; } = string.Empty;
-
-        public string SelectedTeacher { get; set; } = string.Empty;
-
-        public string SelectedSubject { get; set; } = string.Empty;
-
-        public DateTime? DateBefore { get; set; }
-
-        public DateTime? DateAfter { get; set; }
-
-        public string SortBy { get; set; } = "date";
-
-        public bool SortDescending { get; set; } = true;
-
-        private int pageNumber = 1;
-
-        private int pageSize = maxPerPage;
-
-        public int PageNumber
-        {
-            get => pageNumber;
-            set => pageNumber = ClampPageNumber(value);
-        }
-
-        public int PageSize
-        {
-            get => pageSize;
-            set => pageSize = ClampPageSize(value);
-        }
     }
 }
---
+++ LearnLink.Core/Models/ListFilterModel.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using static LearnLink.Core.Constants.PaginationConstants;
+
+namespace LearnLink.Core.Models
+{
+    [ValidateNever]
+    public abstract class ListFilterModel
+    {
+        private int pageNumber = 1;
+
+        private int pageSize = maxPerPage;
+
+        public string SelectedStudent { get; set; } = string.Empty;
+
+        public string SelectedTeacher { get; set; } = string.Empty;
+
+        public string SelectedSubject { get; set; } = string.Empty;
+
+        public DateTime? DateBefore { get; set; }
+
+        public DateTime? DateAfter { get; set; }
+
+        public string SortBy { get; set; } = "date";
+
+        public bool SortDescending { get; set; } = true;
+
+        public int PageNumber
+        {
+            get => pageNumber;
+            set => pageNumber = ClampPageNumber(value);
+        }
+
+        public int PageSize
+        {
+            get => pageSize;
+            set => pageSize = ClampPageSize(value);
+        }
+    }
+}
